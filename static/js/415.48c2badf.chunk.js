e8c895ad1aef02b6dc3156668d813e48c0cbbe0d
maxieo <maxieodev@proton.me>
2022-12-07 15:05:30 +0000

    Deploying to gh-pages from @ maxieo/goit-react-hw-08-phonebook@b91f1072c26fc17c5caa16dfbc01a935e2b8f0ef 🚀

---
+++ static/js/415.48c2badf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{415:function(e,o,n){n.r(o),n.d(o,{default:function(){return c}});var t=n(184);function c(){return(0,t.jsx)("div",{children:(0,t.jsx)("h1",{children:"Welcome to Phonebook"})})}}}]);
+//# sourceMappingURL=415.48c2badf.chunk.js.map
